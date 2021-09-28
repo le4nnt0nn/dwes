db993e047a0d233e59d0899f535da09afe82bd16
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-28 20:46:59 +0200

    chequeador.js : init

--- Trimestre1/Node/ComenzandoNode/Chequeador/chequeador.js
+++ Trimestre1/Node/ComenzandoNode/Chequeador/chequeador.js
@@ -1,11 +1,28 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-/* Chequea si la cadena de texto contiene al menos 3 "x" */
+/* Hace chequeo de un login */
+const usuario = prompt('Inserta un nombre de usuario: ')
+const pass = prompt('Inserta la password: ');
 
-const cadena = prompt('Inserta la cadena: ');
-
-if(!cadena.includes('xxx')) {
-    console.log('La cadena no incluye 3 x');
+if(!pass.includes('xxx')) {
+    console.log('La password no contiene al menos 3 x, no es segura');
+    process.exit();
 } else {
-    console.log('Hay 3 X!')
+    console.log('---------------------');
+    console.log('Registro completado! Ahora, logueate')
+    console.log('---------------------');   
 }
+
+
+const usuarioInput = prompt('Inserta tu nombre de usuario: ')
+const passInput = prompt('Inserta tu password: ');
+
+if(usuarioInput!==usuario || passInput!==pass) {
+    console.log('Uno de los datos son erroneos, saliendo del programa...');
+} else {
+    console.log('Bienvenid@, '+String(usuario));
+}
+
+
+
+
