914665cb1d1d036855b8bc68d521862e67abf290
Antonio Cortés <ditacj78@gmail.com>
2021-10-05 20:34:54 +0200

    entregable 05-10-2021 : created upvotes-downvotes.js

---
+++ Trimestre1/Node/entregable 05-10-2021/upvotes-downvotes.js
@@ -0,0 +1,22 @@
+const votes1 = {
+    upvotes: 13,
+    downvotes: 0,
+};
+
+const votes2 = {
+    upvotes: 2,
+    downvotes: 33,
+};
+
+const votes3 = {
+    upvotes: 132,
+    downvotes: 132,
+};
+
+function getVoteCount(ob1) {
+    return ob1.upvotes-ob1.downvotes
+}
+
+console.log(getVoteCount(votes1))
+console.log(getVoteCount(votes2))
+console.log(getVoteCount(votes3))
