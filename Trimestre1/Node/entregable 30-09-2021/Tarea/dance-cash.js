d33bd8c5131de05f4e5788849270b840ebcd6f60
Antonio Cortés <ditacj78@gmail.com>
2021-10-05 09:21:33 +0200

    dance-cash.js : simplified logic and formatted with prettier

--- Trimestre1/Node/entregable 30-09-2021/Tarea/dance-cash.js
+++ Trimestre1/Node/entregable 30-09-2021/Tarea/dance-cash.js
@@ -1,29 +1,43 @@
 /**Create a program that converts a customer's PIN number to its dance equivalent.
  *  There is one dance move per digit in the PIN number. A list of dance moves is given in the code. */
 
- const MOVES = ['Shimmy', 'Shake', 'Pirouette', 'Slide', 'Box Step', 'Headspin', 'Dosado', 'Pop', 'Lock', 'Arabesque'];
+const MOVES = [
+  "Shimmy",
+  "Shake",
+  "Pirouette",
+  "Slide",
+  "Box Step",
+  "Headspin",
+  "Dosado",
+  "Pop",
+  "Lock",
+  "Arabesque",
+];
 
- function danceConvert(pin) {
-    let arr = ('' + pin).split('').map(function(digit)  {return +digit;});
-    // Convierte el pin en un array de String
-    const pinStringArray = arr.map(x => x.toString());
-    // Regex que chequea si el pin lleva cualquier letra
-    let regex = new RegExp('[a-zA-Z]');
-    const checkReg = pinStringArray.forEach(x => regex.test(x) === true ? console.log('Invalid input.') || process.exit(1) : false);
-    // Si checkRegex es true, sale del programa
-    if(checkReg === true) {
-        process.exit(1);
+function danceConvert(pin) {
+  // Regex que chequea si el pin lleva cualquier caracter que no sea numero
+  //y exceda la longitud de 4
+  let regex = new RegExp(/^[0-9]{4}$/);
+  if (!regex.test(pin)) return "Invalid input.";
+  /**
+   * Each dance move will be selected from a list by index based on the current digit's value plus that digit's index value.
+   * If this value is greater than the last index value of the dance list, it should cycle to the beginning of the list.
+   */
+
+  // Array a devolver
+  const dancePin = [];
+
+  for (let i = 0; i < pin.length; i++) {
+    // Transformo el indice de pin en numero para que se pueda ser sumado
+    let ind = Number(pin[i]) + i;
+    if (ind > 9) {
+      // resta el indice ind = ind - 10
+      ind -= 10;
     }
-    /**
-     * Each dance move will be selected from a list by index based on the current digit's value plus that digit's index value. 
-     * If this value is greater than the last index value of the dance list, it should cycle to the beginning of the list.
-     */
-    // TODO lógica indice
-    const newPinDance = pinStringArray.map(x => x = MOVES[parseInt(x)]);
-    console.log(newPinDance);
+    // Agrega a dancePin
+    dancePin.push(MOVES[ind]);
+  }
+  return dancePin;
 }
 
-danceConvert('9999');
-
- // Convierte los numeros pin en movimientos de baile aleatorios del array MOVES
-//x = MOVES[Math.floor(Math.random()*MOVES.length)]
+console.log(danceConvert("9912"));
