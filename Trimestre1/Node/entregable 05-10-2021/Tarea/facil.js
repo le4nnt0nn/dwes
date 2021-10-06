e218063c396c5b415dfa6113b916be71cb1a8c4c
Antonio Cortés <ditacj78@gmail.com>
2021-10-06 19:56:49 +0200

    facil.js : formatted with prettier

--- Trimestre1/Node/entregable 05-10-2021/Tarea/facil.js
+++ Trimestre1/Node/entregable 05-10-2021/Tarea/facil.js
@@ -1,29 +1,29 @@
-/**Write a function that converts an object into an array, 
+/**Write a function that converts an object into an array,
  * where each element represents a key-value pair in the form of an array. */
 
 const persona = {
-    nombre: 'antonio',
-    apellido: 'cortes',
-    edad: 19,
-}
+  nombre: 'antonio',
+  apellido: 'cortes',
+  edad: 19,
+};
 
 // Transformar un objeto en un array, cada elemento será clave-valor:
 function toArray(obj) {
-    const entries = Object.entries(obj);
+  const entries = Object.entries(obj);
 
-    return entries;
+  return entries;
 }
 
 console.log(toArray(persona));
 
 // Transformar un objeto en un array con dos elementos [claves, valores] :
 function keysAndValues(obj) {
-	const keys = Object.keys(obj);
-    const values = Object.values(obj);
+  const keys = Object.keys(obj);
+  const values = Object.values(obj);
 
-    let arrayDevuelto = [keys, values];
+  let arrayDevuelto = [keys, values];
 
-    return arrayDevuelto;
+  return arrayDevuelto;
 }
 
 console.log(keysAndValues(persona));
