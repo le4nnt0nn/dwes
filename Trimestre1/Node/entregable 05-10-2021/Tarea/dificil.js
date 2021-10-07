b090ab9b973cd9451c18ad9fc58efb54da880d25
Antonio Cortés <ditacj78@gmail.com>
2021-10-07 18:00:50 +0200

    Tarea : init dificil.js

---
+++ Trimestre1/Node/entregable 05-10-2021/Tarea/dificil.js
@@ -0,0 +1,8 @@
+// Comprobar si dos objetos son iguales de manera recursiva.
+
+function isSimilar(arrX, arrY) {
+	
+	return isSimilar(arrX, arrY)
+}
+
+console.log(isSimilar(["cars", "trains", ["roads", ["railways"]]], ["cars", "trains", ["roads", ["railways"]]]));
