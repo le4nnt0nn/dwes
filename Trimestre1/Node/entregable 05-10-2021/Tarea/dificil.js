14b3b45c0eae35949d556272275cc9d7c7d75a7c
Antonio <ditacj78@gmail.com>
2021-10-09 18:25:06 +0200

    dificil.js : created logic

--- Trimestre1/Node/entregable 05-10-2021/Tarea/dificil.js
+++ Trimestre1/Node/entregable 05-10-2021/Tarea/dificil.js
@@ -1,8 +1,27 @@
 // Comprobar si dos objetos son iguales de manera recursiva.
 
 function isSimilar(arrX, arrY) {
-	
-	return isSimilar(arrX, arrY)
+
+	// Si las keys de ambos no son iguales, return false
+	if (Object.keys(arrX).toString() !== Object.keys(arrY).toString()) {
+		return false
+	} else if (arrX.length !== arrY.length) {
+		return false
+	};
+	// Recorre cada elemento de arrX
+	for (let element in arrX) {
+		// Si el tipo de los elementos son object y los elementos de ambos arrays no son iguales, return false
+		// Uso del método isSimilar de forma recursiva
+		if (typeof(arrX[element]) === 'object' && !isSimilar(arrX[element], arrY[element])) {
+			return false
+		};
+		// Si el tipo de los elementos no son objetos y los elementos no coinciden, return false
+		if (typeof(arrX[element]) !== 'object' && arrX[element] !== arrY[element]) {
+			return false
+		};
+	};
+
+	return true;
 }
 
-console.log(isSimilar(["cars", "trains", ["roads", ["railways"]]], ["cars", "trains", ["roads", ["railways"]]]));
+console.log(isSimilar(['Gabri', 'Antonio', 'Bob'], ['Gabri', 'Antonio', 'Bob']));
