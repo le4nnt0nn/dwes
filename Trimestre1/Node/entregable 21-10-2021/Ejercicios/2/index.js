9c44ca225a4fd9a246cca623e6cf13b1a75412c7
le4nnt0nn <ditacj78@gmail.com>
2021-10-21 19:40:04 +0200

    created index.js

---
+++ Trimestre1/Node/entregable 21-10-2021/Ejercicios/2/index.js
@@ -0,0 +1,19 @@
+const faker = require("faker");
+const chalk = require("chalk");
+const { DateTime } = require("luxon");
+
+function changeWow() {
+    if(DateTime.now().get('second') === 00 || DateTime.now().get('second') % 10 == 0) {
+        return chalk.green(DateTime.fromISO(DateTime.now()).toFormat("dd-MM-yyyy HH:mm:ss"))
+    } else {
+        return DateTime.fromISO(DateTime.now()).toFormat("dd-MM-yyyy HH:mm:ss")
+    }
+}
+
+const randomColor = faker.commerce.color();
+
+console.log(`
+
+    ${chalk.keyword(randomColor)('MegaCl0ck')} !
+`)
+setInterval(() => process.stdout.write(`${changeWow()}\r`), 1000);
