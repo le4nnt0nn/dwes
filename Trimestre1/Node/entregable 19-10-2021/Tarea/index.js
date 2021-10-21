9b6d1885a0fe99d113ff3138674f14ee15da4d8a
le4nnt0nn <ditacj78@gmail.com>
2021-10-21 11:19:38 +0200

    index.js : added changeNow logic

--- Trimestre1/Node/entregable 19-10-2021/Tarea/index.js
+++ Trimestre1/Node/entregable 19-10-2021/Tarea/index.js
@@ -2,24 +2,21 @@ const faker = require("faker");
 const chalk = require("chalk");
 const { DateTime } = require("luxon");
 
-//* UTILS
-// process.stdout.write("\r");
-// const humanReadable = date.toLocaleString(DateTime.DATETIME_MED);
-// const date = DateTime.fromISO(DateTime.now()).toFormat('dd-MM-yyyy HH:mm:ss');
-// const date = DateTime.fromISO(DateTime.now().plus(1000)).toFormat("dd-MM-yyyy HH:mm:ss");
-// setInterval(() => process.stdout.write(`${DateTime.fromISO(DateTime.now().plus(1000)).toFormat("dd-MM-yyyy HH:mm:ss")}\r`), 1000);
-// setInterval(() => process.stdout.write(`${DateTime.now().plus(1000).toSeconds().toFixed()}\r`), 1000);
-
-// TODO - Lógica de colorChange
-function colorChange() {
-    if(DateTime.fromISO(DateTime.now().toSeconds().toFixed()) === 0) {
-        return chalk.green(DateTime.fromISO(DateTime.now().plus(1000)).toFormat("dd-MM-yyyy HH:mm:ss"));
-    }else {
-        return DateTime.fromISO(DateTime.now().plus(1000)).toFormat("dd-MM-yyyy HH:mm:ss");
+function changeWow() {
+    if(DateTime.now().get('second') === 00 || DateTime.now().get('second') % 10 == 0) {
+        return chalk.green(DateTime.fromISO(DateTime.now()).toFormat("dd-MM-yyyy HH:mm:ss"))
+    } else {
+        return DateTime.fromISO(DateTime.now()).toFormat("dd-MM-yyyy HH:mm:ss")
     }
 }
 
-setInterval(() => process.stdout.write(`${colorChange()}\r`), 1000);
+const randomColor = faker.commerce.color();
+
+console.log(`
+
+    ${chalk.keyword(randomColor)('MegaCl0ck')} !
+`)
+setInterval(() => process.stdout.write(`${changeWow()}\r`), 1000);
 
 
 
