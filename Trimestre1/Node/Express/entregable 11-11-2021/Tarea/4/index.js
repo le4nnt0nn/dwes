08c59f5b5b185a56290e551cc22c96ecb885740b
Antonio Cortés <ditacj78@gmail.com>
2021-11-16 19:33:20 +0100

    continue ej4

--- Trimestre1/Node/Express/entregable 11-11-2021/Tarea/4/index.js
+++ Trimestre1/Node/Express/entregable 11-11-2021/Tarea/4/index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
+const object = {1 : 'valor', 2: 'valor', 3:'valor'}
+const entradas = Object.entries(object);
 // TODO - HACER EJERCICIO
 app.get(`/body/:object`, function (req, res) {
-    console.log(req.params.object);
-    res.send(`<ul>
-                <li>${req.params.object}</li>
-                <li></li>
-            </ul>`)
+    const entradas2 = Object.entries(req.params.object)
+    console.log(entradas);
+    for (let index = 0; index < entradas.length; index++) {
+      console.log(index)
+      res.send(`<li>${entradas[index]}</li>`)
+    }
 });
 
 app.get('/:', (req, res) => {
