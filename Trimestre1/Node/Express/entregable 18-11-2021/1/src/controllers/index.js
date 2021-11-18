8f0b9e50e295e63cc92be93e89bacc55bb6050e7
Antonio Cortés <ditacj78@gmail.com>
2021-11-18 20:18:54 +0100

    updated ej1

--- Trimestre1/Node/Express/entregable 18-11-2021/1/src/controllers/index.js
+++ Trimestre1/Node/Express/entregable 18-11-2021/1/src/controllers/index.js
@@ -2,12 +2,6 @@ function rootController(req, res) {
     res.status(200).send('Hello I\'m here!')
 }
 
-function nameController(req, res) {
-    const { name = 'Gabriel' } = req.query;
-    res.status(200).send(`Hello my name is ${name}`);
-}
-
 module.exports = {
     rootController,
-    nameController,
 }
