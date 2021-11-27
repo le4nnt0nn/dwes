2fa685311bfcc51024724a891e6e77a5c0ad757b
Antonio Cortés <71550295+le4nnt0nn@users.noreply.github.com>
2021-11-27 17:07:06 +0100

    added http methods

--- Trimestre1/Node/Express/entregable 23-11-2021/1/src/routes/index.js
+++ Trimestre1/Node/Express/entregable 23-11-2021/1/src/routes/index.js
@@ -5,11 +5,21 @@ const userRouter = require('./user');
 
 const router = express.Router();
 
-router.get('/', rootController);
+// Para probar en local
+/* router.get('/', rootController);
 router.get('/users', userRouter);
 router.get('/users/:id', userRouter);
 router.get('/users/add/:object', userRouter);
 router.get('/users/edit/:id/:object', userRouter);
-router.get('/users/remove/:id', userRouter);
+router.get('/users/remove/:id', userRouter); */
+
+// Para Postman
+
+router.get('/', rootController);
+router.get('/users', userRouter);
+router.get('/users/:id', userRouter);
+router.post('/users/add/:object', userRouter);
+router.put('/users/edit/:id/:object', userRouter);
+router.delete('/users/remove/:id', userRouter)
 
 module.exports = router;
--- Trimestre1/Node/Express/entregable 23-11-2021/1/src/routes/user.js
+++ Trimestre1/Node/Express/entregable 23-11-2021/1/src/routes/user.js
@@ -5,10 +5,21 @@ const { showUsers, showUser, addUser, editUser, removeUser } = require('../contr
 
 const router = express.Router();
 
-router.get('/users', showUsers);
+// Para probar en local
+
+/* router.get('/users', showUsers);
 router.get('/users/:id', showUser)
 router.get('/users/add/:object', addUser)
 router.get('/users/edit/:id/:object', editUser)
-router.get('/users/remove/:id', removeUser)
+router.get('/users/remove/:id', removeUser) */
+
+// Para Postman
+
+router.get('/users', showUsers);
+router.get('/users/:id', showUser)
+router.post('/users/add/:object', addUser)
+router.put('/users/edit/:id/:object', editUser)
+router.delete('/users/remove/:id', removeUser)
+
 
 module.exports = router;
