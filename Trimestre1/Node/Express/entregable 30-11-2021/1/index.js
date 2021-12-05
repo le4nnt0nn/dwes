6d56688c7b92e7a25b3c45590b34ce138d31bf61
Antonio Cortés <71550295+le4nnt0nn@users.noreply.github.com>
2021-12-05 12:02:45 +0100

    fixed messages

--- Trimestre1/Node/Express/entregable 30-11-2021/1/index.js
+++ Trimestre1/Node/Express/entregable 30-11-2021/1/index.js
@@ -7,19 +7,19 @@ const hash = '$2b$05$emcUjS2uekm95IK/5XL9ge36B3f7JA/9ThTfL4LobQUYe/AZrEr8y';
 
 app.get('/admin', function (req, res, next) {
     res.status(401).send({
-        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabera de la petición'
+        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabecera de la petición'
     });
 })
 
 app.get('/admin/password', function (req, res, next) {
     res.status(401).send({
-        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabera de la petición'
+        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabecera de la petición'
     });
 })
 
 app.get('/admin/password/*', function (req, res, next) {
     res.status(401).send({
-        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabera de la petición'
+        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabecera de la petición'
     });
 })
 
@@ -37,7 +37,7 @@ app.get('/admin/password:secret', function (req, res, next) {
     else next()
 }, function (req, res, next) {
     res.status(401).send({
-        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabera de la petición'
+        code: 401, message: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro password en la cabecera de la petición'
     });
 })
 
