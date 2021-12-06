19040926212258ad46e5149fee45e9cff9ba44ad
Antonio <ditacj78@gmail.com>
2021-12-06 17:09:06 +0100

    notas.js : changed name for id

--- Trimestre1/Node/Trabajo-Estructurado/src/routes/notas.js
+++ Trimestre1/Node/Trabajo-Estructurado/src/routes/notas.js
@@ -5,6 +5,6 @@ const { showNotes, showNote } = require('../controllers/notas');
 const router = express.Router();
 
 router.get('/notas', showNotes);
-router.get('/notas/:name', showNote);
+router.get('/notas/:id', showNote);
 
 module.exports = router;
