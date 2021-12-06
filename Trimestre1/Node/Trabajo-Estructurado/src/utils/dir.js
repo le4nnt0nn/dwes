13ab081a83ce787e599a1738bf83eb881fb302b2
Antonio <ditacj78@gmail.com>
2021-12-06 15:45:50 +0100

    dir.js : added findContentNotes

--- Trimestre1/Node/Trabajo-Estructurado/src/utils/dir.js
+++ Trimestre1/Node/Trabajo-Estructurado/src/utils/dir.js
@@ -3,12 +3,21 @@ const fs = require("fs/promises");
 const folderName = 'notes';
 var allNotes = [];
 let files;
+let content;
+const currentNotes = __dirname;
 
 async function findNotes() {
     files = await fs.readdir(`${__dirname}/${folderName}`)
     return files;
 };
 
+async function findContentNotes(f) {
+    content = await fs.readFile(`${__dirname}/${folderName}/${f}`)
+    return content;
+}
+
 module.exports = {
     findNotes,
+    currentNotes,
+    findContentNotes,
 }
