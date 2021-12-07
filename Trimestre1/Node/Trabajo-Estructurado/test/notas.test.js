b5bf2f879ce4ffd23377d5a7f9bbc74a7d0d40e2
Antonio <ditacj78@gmail.com>
2021-12-07 10:05:08 +0100

    added test and installed sinon

--- Trimestre1/Node/Trabajo-Estructurado/test/notas.test.js
+++ Trimestre1/Node/Trabajo-Estructurado/test/notas.test.js
@@ -1,5 +1,38 @@
-const { myNotas } = require('../src/controllers/notas')
+const sinon = require('sinon');
+const { showNote } = require('../src/controllers/notas');
 
-test('should work', () => {
-    expect(myNotas()).toStrictEqual();
+
+const mockReq = (id) => ({
+    params: {
+        id,
+    }
+});
+
+const mockRes = () => ({
+    status: function(s) { this.statusCode=s; return this; },
+    statusCode: 200,
+    send: function(b) { this.body=b; return this;},
+});
+
+describe('getNoteById 200 Ok', () => {
+    it('should getNoteById', (done) => {
+        const noteId = 1;
+        const callback = sinon.stub();
+        const res = showNote(mockReq(noteId), mockRes());
+        console.log(JSON.stringify(res, null, 2));
+        expect(res.statusCode).toBe(200);
+        expect(res.body.id).toBe(noteId);
+        done();
+    });
+});
+
+describe('getNoteById Error 404', () => {
+    it('should getNoteById', (done) => {
+        const noteId = 123;
+        const callback = sinon.stub();
+        const res = showNote(mockReq(noteId), mockRes());
+        expect(res.statusCode).toBe(404);
+        expect(res.body.message).toBe('Nota no encontrada');
+        done();
+    });
 });
