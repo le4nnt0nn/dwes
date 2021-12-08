021e96620ee62bd05b94d5855465261f67363771
Antonio <ditacj78@gmail.com>
2021-12-08 12:24:27 +0100

    added pagination and sort

--- Trimestre1/Node/Express/entregable 02-12-2021/2/src/utils/filter.js
+++ Trimestre1/Node/Express/entregable 02-12-2021/2/src/utils/filter.js
@@ -4,6 +4,20 @@ function filterObject(items, filters) {
     ));
 }
 
+function sortObject(items) {
+    return items.sort(function (a, b) {
+        if (a.name < b.name) { return -1; }
+        if (a.name > b.name) { return 1; }
+        return 0;
+    })
+}
+
+function paginateObject(items, page_size, page_number) {
+    return items.slice((page_number - 1) * page_size, page_number * page_size);
+}
+
 module.exports = {
     filterObject,
+    sortObject,
+    paginateObject,
 };
