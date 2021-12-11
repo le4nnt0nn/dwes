78b01bb5bec30d845e358c1dc2f969a60f3359e8
Antonio <ditacj78@gmail.com>
2021-12-11 12:41:44 +0100

    index.js : added routes

--- Trimestre1/Node/Trabajo-Estructurado/src/routes/index.js
+++ Trimestre1/Node/Trabajo-Estructurado/src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const { rootController } = require('../controllers');
 const notasRouter = require('./notas'); 
+const uploadRouter = require('./upload'); 
+const dowRouter = require('./download');
 
 const router = express.Router();
 
@@ -11,6 +13,9 @@ router.get('/notas/:id', notasRouter);
 router.post('/notas/:name', notasRouter);
 router.put('/notas/:id', notasRouter);
 router.delete('/notas/:id', notasRouter);
+router.post('/upload', uploadRouter);
+router.get('/download', dowRouter);
+router.get('/download/:name', dowRouter);
 router.post('/login', notasRouter);
 
 module.exports = router;
