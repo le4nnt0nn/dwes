9ef44b01d8bb2ca6edaae6760f36096798424a3a
Antonio Cortés <ditacj78@gmail.com>
2022-01-23 19:53:19 +0100

    modified and removed files

--- Trimestre2/Mongoose/EjemploAPI/src/routes/index.js
+++ Trimestre2/Mongoose/EjemploAPI/src/routes/index.js
@@ -1,14 +1,3 @@
 const express = require('express');
 
-const { rootController, nameController } = require('../controllers');
-const bookRouter = require('./book'); 
-const flyRouter = require('./fly'); 
 
-const router = express.Router();
-
-router.get('/', rootController);
-router.get('/name', nameController);
-router.use('/book', bookRouter);
-router.use('/fly', flyRouter);
-
-module.exports = router;
