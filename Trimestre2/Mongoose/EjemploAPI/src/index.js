f8aeb1869c888f46ce68c525448d69e66e9447d2
Antonio <ditacj78@gmail.com>
2022-01-30 11:27:56 +0100

    index.js: added new route

--- Trimestre2/Mongoose/EjemploAPI/src/index.js
+++ Trimestre2/Mongoose/EjemploAPI/src/index.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const Product = require('./routes/product');
+const WeatherData = require('./routes/weatherdata');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/product', Product);
+app.use('/weatherdata', WeatherData);
 
 module.exports = app
