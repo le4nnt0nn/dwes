999f5af98a2157cb1100c99dbebd28e1f91408e8
Antonio <ditacj78@gmail.com>
2022-01-31 19:51:53 +0100

    index.js: added routes

--- Trimestre2/Mongoose/EjemploAPI/src/index.js
+++ Trimestre2/Mongoose/EjemploAPI/src/index.js
@@ -4,12 +4,28 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const Product = require('./routes/product');
-const WeatherData = require('./routes/weatherdata');
+const Grade = require('./routes/grades');
+const Data = require('./routes/weatherdata');
+const Comment = require('./routes/comments');
+const Movie = require('./routes/movies');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// NOTA: para usar las distintas bbdd, cambiar parámetro en .env
+
+//myFirstDatabase
 app.use('/product', Product);
-app.use('/weatherdata', WeatherData);
+
+//sample_weatherdata
+app.use('/data', Data);
+
+//sample_mflix
+app.use('/comments', Comment);
+
+app.use('/movies', Movie);
+
+//sample_training
+app.use('/grades', Grade);
 
 module.exports = app
